daeb25f303a4ceecc7ec0902451e4985391c9af9
Lucas <18827394752@163.com>
2023-12-25 15:00:30 +0800

    12.25

---
+++ Assets/Scripts/Framework/Utils/Test.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Test<T> where T :new()
+{
+    private static T instance;
+    private static object obj;
+
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                lock (obj)
+                {
+                    if(instance == null)
+                        instance = new T();
+                }
+            }
+            return instance;
+        }
+    }
+}
+
+public class TestMonoSingleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectsOfType(typeof(T)) as T;
+                if(instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    instance = obj.AddComponent(typeof(T)) as T;
+                    obj.name = typeof(T).Name;    
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (instance == null)
+            instance = this as T;
+        else
+            Destroy(this.gameObject);
+    }
+}
+
+public static class Test2<T>  where T :new()
+{
+    private static T instance;
+    private static object obj = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (obj)
+                {
+                    if (instance == null)
+                        instance = new T();
+                }
+            }
+            return instance;
+        }
+    }
+
+}
+
+public class Mono2Singleton<T> : MonoBehaviour where T : Mono2Singleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = GameLaunch.FindObjectOfType(typeof(T)) as T;
+                if (instance == null)
+                {
+                    instance = new GameObject((typeof(T)).Name).AddComponent<T>();
+                }
+
+                instance.Init();
+            }
+
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+            instance = this as T;
+        this.Init();
+    }
+
+    public virtual void Init() { }
+}
+
